d9e94ec09412e1b8acc12759148d0ee13d56fed0
Maxim Willinger <1203@htl.rennweg.at>
2023-09-01 23:25:21 +0200

    Updated the Player animations

--- code/level.py
+++ code/level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-# from debug import debug
+from debug import debug
 from support import *
 from random import choice
 
@@ -50,6 +50,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.status)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
