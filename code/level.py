85b37fe37b305cdba88590b7b9e343ccf61f2613
Maxim Willinger <1203@htl.rennweg.at>
2023-09-02 22:38:38 +0200

    Made the player weapons

--- code/level.py
+++ code/level.py
@@ -2,9 +2,10 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-from debug import debug
+import debug
 from support import *
 from random import choice
+from weapon import Weapon
 
 
 class Level:
@@ -15,6 +16,9 @@ class Level:
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+
+        # attack sprites
+        self.current_attack = None
 
         # sprite setup
         self.create_map()
@@ -44,13 +48,22 @@ class Level:
                         if style == 'object':
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
+
 
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.status)
+        debug.debug(self.player.status)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame
+import sys
 from settings import *
 from level import Level
 
